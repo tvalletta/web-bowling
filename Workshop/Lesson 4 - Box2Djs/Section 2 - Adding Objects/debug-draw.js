63665d0087faf84b3892b79ae5e94879d1290bb8
Thomas A. Valletta <github.tom@vallettas.com>
2011-11-23 10:15:50 -0700

    Box2Djs lessons

---
+++ Workshop/Lesson 4 - Box2Djs/Section 2 - Adding Objects/debug-draw.js
@@ -0,0 +1,14 @@
+function debugDraw(canvas, world) {
+	var ctx 			= canvas.getContext("2d"),
+		b2DebugDraw 	= Box2D.Dynamics.b2DebugDraw;
+	
+	var debugDraw = new b2DebugDraw();
+	debugDraw.SetSprite(ctx);
+	debugDraw.SetDrawScale(scale);
+	debugDraw.SetFillAlpha(0.3);
+	debugDraw.SetLineThickness(1.0);
+	debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+	
+	world.SetDebugDraw(debugDraw);
+	world.DrawDebugData();
+}
