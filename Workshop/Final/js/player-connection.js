53ec9716ff7924b447b8642e1f48446106cbc20e
Gabriel Dayley <gabriel.dayley+git@gmail.com>
2011-11-23 10:17:55 -0700

    tweaks

--- Workshop/Final/js/player-connection.js
+++ Workshop/Final/js/player-connection.js
@@ -1,16 +1,20 @@
 var socket,
-//	server = 'http://localhost:3008';
-	server = 'http://10.109.38.135:3008';
+	server = 'http://127.0.0.1:3008';  //Be sure to use your servers ip address, and the port your server is listening on
 
 
 var connect = function(callback) {
-//	console.log("Attempting to connect to " + server);
+	console.log("Attempting to connect to " + server);
+
+    //Connect to server
 	socket = io.connect(server);
+
+    //Listen for ack from server that we have connected
 	socket.on('ack', function() {
-//		console.log("Sending identification");
-		socket.emit('identify', 'P', function(data) {
-//			console.log('Recieved confirmation');
-			if (data && callback) callback();
-		});
+	    console.log('You have connected');
+
+	    //Execute the callback, which should be our setReady function
+	    if(callback) {
+	        callback();     //This should unhide our bowling ball
+	    }
 	});
 };
--- js/display-connection.js
+++ js/display-connection.js
@@ -1,17 +1,20 @@
 var socket,
-	server = 'http://127.0.0.1:3008';
+	server = 'http://127.0.0.1:3008';  //Be sure to use your servers ip address, and the port your server is listening on
+
 
 var connect = function(callback) {
 	console.log("Attempting to connect to " + server);
+
+    //Connect to server
 	socket = io.connect(server);
+
+    //Listen for ack from server that we have connected
 	socket.on('ack', function() {
-		console.log("Sending identification");
-		socket.emit('identify', 'D', function(data) {
-			console.log("Recieved confirmation");
-			if (data && callback) callback();
-		});
-	});
-	socket.on('release', function(data) {
-		release(data);
+	    console.log('You have connected');
+
+	    //Execute the callback, which should be our setReady function
+	    if(callback) {
+	        callback();     //This should unhide our bowling ball
+	    }
 	});
 };
