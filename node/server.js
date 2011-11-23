8d1e64ec0319f0e2e58cab58f2201c8133237f7e
Thomas A. Valletta <github.tom@vallettas.com>
2011-11-23 12:07:33 -0700

    Final seems to work.  Cleaning up directory structure.

--- node/server.js
+++ node/server.js
@@ -19,36 +19,13 @@ io.configure('development', function(){
 	]);
 });
 
-var displays = [],
-	players = [];
-
 io.sockets.on('connection', function (socket) {
-    var clientId = socket.id;
-
+	console.log('Connection Made', socket.id);
     socket.emit('ack', { message: 'You are connected.' });
-	
-	socket.on('identify', function(type, callback) {
-		console.log('Identify type: ' + type);
-		if (type === "D") {
-			console.log("Display Added.")
-			displays.push(socket);
-		}
-		else if (type === "P") {
-			console.log("Player Added.")
-			players.push(socket);
-		}
-		else {
-			if (callback) callback('false');
-		}
 		
-		if (callback) callback('true');
-	});
-	
     socket.on('release', function (data) {
-        for (var i = 0, cnt = displays.length; i < cnt; i++) {
-			console.log('Sent to Display: ' + displays[i].id);
-			displays[i].emit('release', data);
-		}
+		console.log('release', data);
+		socket.broadcast.emit('release', data);
     });
 });
 
