d6f6e336c8309e7bcb3a225efe9191f5b2f4c6c7
Gabriel Dayley <gabriel.dayley+git@gmail.com>
2011-11-29 15:05:49 -0700

    complete L3.1

--- wb/js/display/bowling-alley.js
+++ wb/js/display/bowling-alley.js
@@ -1,52 +1,63 @@
 function BowlingAlley(canvas) {
-    var b2Color         = Box2D.Common.b2Color,
-        b2Math          = Box2D.Common.Math.b2Math,
-        b2Vec2          = Box2D.Common.Math.b2Vec2,
-        b2AABB          = Box2D.Collision.b2AABB,
-        b2BodyDef       = Box2D.Dynamics.b2BodyDef,
-        b2Body          = Box2D.Dynamics.b2Body,
-        b2FixtureDef    = Box2D.Dynamics.b2FixtureDef,
-        b2Fixture       = Box2D.Dynamics.b2Fixture,
-        b2World         = Box2D.Dynamics.b2World,
-        b2Shape         = Box2D.Collision.Shapes.b2Shape,
-        b2MassData      = Box2D.Collision.Shapes.b2MassData,
-        b2PolygonShape  = Box2D.Collision.Shapes.b2PolygonShape,
-        b2CircleShape   = Box2D.Collision.Shapes.b2CircleShape,
-        b2DebugDraw     = Box2D.Dynamics.b2DebugDraw,
+    var b2Color = Box2D.Common.b2Color,
+        b2Math = Box2D.Common.Math.b2Math,
+        b2Vec2 = Box2D.Common.Math.b2Vec2,
+        b2AABB = Box2D.Collision.b2AABB,
+        b2BodyDef = Box2D.Dynamics.b2BodyDef,
+        b2Body = Box2D.Dynamics.b2Body,
+        b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+        b2Fixture = Box2D.Dynamics.b2Fixture,
+        b2World = Box2D.Dynamics.b2World,
+        b2Shape = Box2D.Collision.Shapes.b2Shape,
+        b2MassData = Box2D.Collision.Shapes.b2MassData,
+        b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+        b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+        b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
         b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
-        
-	// Variables for "frames per second", "minimum frames per second", and "scale"
-	// todo: Create fps, fpsMin, and scale variables for Box2Djs (L3.1 b)
 
-	// Box2D world
-	// todo: Create a new Box2D world (L3.1 c)
+    // Variables for "frames per second", "minimum frames per second", and "scale"
+    var fps = 30,
+        fpsMin = 10,
+        scale = 30;
 
-	// Default fixture definition
-	// todo: Define a fixture definition (L3.1 d)
+    // Box2D world
+    var world = new b2World(
+        new b2Vec2(0, 0), // gravity
+        true                        // allow sleep
+    );
 
-	// Dynamic Body
-	// todo: Define a dynamic body (L3.1 e)
+    // Default fixture definition
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 2.0;           // density of default bodies
+    fixDef.friction = 0.5;          // friction of default bodies surfaces
+    fixDef.restitution = 0.2;       // rebound of default bodies collision
 
-	// Canvas element
-	// todo: Create a variable for the canvas element (L3.2 a)
+    // Dynamic Body
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_dynamicBody;
+    // bodyDef.type = b2Body.b2_kinematicBody;
+    // bodyDef.type = b2Body.b2_staticBody;
 
-	// Create shapes
-	// todo: Create and draw random shapes (L3.2 b)
-	// todo: Replace the random shapes call with createPins  (L3.2 d)
+    // Canvas element
+    // todo: Create a variable for the canvas element (L3.2 a)
 
-	// Main loop
-	// todo: create the game's main loop (L3.3 b)
-	
-	this.ball = function(data) {
-		console.log("Ball: " + data);
-		
-		// Variables for x velocity, y velocity, and spin
-		// todo: Create variables for x velocity, y velocity, and spin (L3.4 a)
+    // Create shapes
+    // todo: Create and draw random shapes (L3.2 b)
+    // todo: Replace the random shapes call with createPins  (L3.2 d)
 
-		// The bowling ball
-		// todo: Create a bowling ball (L3.4 b)
+    // Main loop
+    // todo: create the game's main loop (L3.3 b)
 
-		// Angular and linear velocity
-		// todo: Create the ball with angular and linear velocity (L3.4 c)
-	}
+    this.ball = function(data) {
+        console.log("Ball: " + data);
+
+        // Variables for x velocity, y velocity, and spin
+        // todo: Create variables for x velocity, y velocity, and spin (L3.4 a)
+
+        // The bowling ball
+        // todo: Create a bowling ball (L3.4 b)
+
+        // Angular and linear velocity
+        // todo: Create the ball with angular and linear velocity (L3.4 c)
+    }
 }
