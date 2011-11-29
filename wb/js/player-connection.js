9d47f281c306104245d84dd635fab7b4d5402d9d
Gabriel Dayley <gabriel.dayley+git@gmail.com>
2011-11-29 14:59:15 -0700

    complete L2.1

--- wb/js/player-connection.js
+++ wb/js/player-connection.js
@@ -1,13 +1,19 @@
 var socket,
-	server = 'http://127.0.0.1:3008';  //todo: Be sure to use your servers ip address and port (L2.1 a)
+    server = 'http://127.0.0.1:3008';  //todo: Be sure to use your servers ip address and port (L2.1 a)
 
 
 var connect = function(callback) {
-	console.log("Attempting to connect to " + server);
+    console.log("Attempting to connect to " + server);
 
     //Connect to server
-	socket = io.connect(server);
+    socket = io.connect(server);
 
     //Listen for ack from server that we have connected
-    //todo:  Add listener for ack messages (L2.1 b)
+    socket.on('ack', function(data) {
+        console.log(data.message);
+
+        if (callback) {
+            callback();     //Note: this is the function we are passing into connect()
+        }
+    });
 };
