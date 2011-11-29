bd8f1bd0cb831934948715991f5645ebb61adab1
Gabriel Dayley <gabriel.dayley+git@gmail.com>
2011-11-29 15:50:55 -0700

    remove todo calcSwing()

--- wb/js/player.js
+++ wb/js/player.js
@@ -88,10 +88,6 @@ var ballRelease = function(e) {
     var swing = fakeSwing();
     console.log(swing);  //For now just log it!
 
-
-    //Calculate Swing
-    //todo:  call calculate swing (L1.3 b)
-
     //Send data
     //todo:  send swing data to socket (L2.2 a)
 };
